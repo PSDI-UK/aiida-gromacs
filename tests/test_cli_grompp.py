761fe62f2bed413825e7dbfe31622cf4e6540418
“Jas <jas.kalayan@stfc.ac.uk>
2023-08-22 14:00:28 +0000

    added extra test for grompp and ammended mdrun to work for gromacs GPU installation

--- tests/test_cli_grompp.py
+++ tests/test_cli_grompp.py
@@ -74,3 +74,38 @@ def test_launch_grompp2():
     # check the process has finished and exited correctly
     assert prev_calc.process_state == ProcessState.FINISHED
     assert prev_calc.exit_status == 0
+
+
+def test_launch_grompp3():
+    """
+    Run an instance of grompp.
+    """
+    # get input file paths
+    mdp_file = os.path.join(TEST_DIR, "input_files", "grompp3_nvt.mdp")
+    gro_file = os.path.join(TEST_DIR, "input_files", "grompp3_1AKI_minimised.gro")
+    top_file = os.path.join(TEST_DIR, "input_files", "grompp2_1AKI_topology.top")
+    # itp_file = os.path.join(TEST_DIR, "input_files", "1AKI_restraints.itp")
+
+    subprocess.check_output(
+        [
+            "gmx_grompp", 
+            "-f", 
+            mdp_file, 
+            "-c", 
+            gro_file, 
+            "-r", 
+            gro_file, 
+            "-p", 
+            top_file, 
+            "-o", 
+            "1AKI_nvt.tpr", 
+        ]
+    )
+    # append run process to qb
+    # pylint: disable=unused-variable
+    qb = searchprevious.build_query()
+    # pylint: disable=unsubscriptable-object
+    prev_calc = qb.first()[0]
+    # check the process has finished and exited correctly
+    assert prev_calc.process_state == ProcessState.FINISHED
+    assert prev_calc.exit_status == 0
