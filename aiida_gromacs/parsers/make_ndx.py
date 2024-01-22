326b08fc688017b5e75c1ba4ce7e5dfc8d260802
“Jas <jas.kalayan@stfc.ac.uk>
2024-01-22 13:07:27 +0000

    parsers to changed to convert potential relative path to absolute path required for copy_tree

--- aiida_gromacs/parsers/make_ndx.py
+++ aiida_gromacs/parsers/make_ndx.py
@@ -39,7 +39,7 @@ class Make_ndxParser(Parser):
         :returns: an exit code, if parsing fails (or nothing if parsing succeeds)
         """
         # the directory for storing parsed output files
-        output_dir = self.node.get_option("output_dir")
+        output_dir = Path(self.node.get_option("output_dir"))
         # Map output files to how they are named.
         outputs = ["stdout"]
         output_template = {
@@ -72,6 +72,6 @@ class Make_ndxParser(Parser):
 
         # If not in testing mode, then copy back the files.
         if "PYTEST_CURRENT_TEST" not in os.environ:
-            self.retrieved.copy_tree(Path(output_dir))
+            self.retrieved.copy_tree(output_dir)
 
         return ExitCode(0)
