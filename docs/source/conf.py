f1e9faa5d4a26b5253ee789efbccf5a809ef3a47
“Jas <jas.kalayan@stfc.ac.uk>
2024-05-22 14:15:16 +0000

    changed RTD theme to furo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -139,9 +139,9 @@ pygments_style = "sphinx"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    "display_version": True,
-}
+# html_theme_options = {
+#     "display_version": True,
+# }
 
 html_theme = "furo"
 # Add any paths that contain custom themes here, relative to this directory.
